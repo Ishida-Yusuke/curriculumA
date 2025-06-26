8a8c0b10a677e6a856271f9daf960ad6137d0983
yusuke <yusuke@localhost>
2025-06-26 14:20:19 +0900

    a

---
+++ src/curriculumA/Qes1_13.java
@@ -0,0 +1,68 @@
+package curriculumA;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//バイト型
+		byte var ;
+		//短整数型
+		short s_var ;
+		//整数型
+		int i_var ;
+		//長整数型
+		long l_var ;
+		//単精度浮動小数点数型
+		float f_var ;
+		//倍精度浮動小数点数型
+		double d_var ;
+		//文字型
+		char c_var ;
+		//文字列型
+		String S_var ;
+		//ブーリアン型
+		boolean b_var ;
+
+		//バイト型
+		var = 0;
+		//短整数型
+		s_var = 0;
+		//整数型
+		i_var = 0;
+		//長整数型
+		l_var = 0l;
+		//単精度浮動小数点数型
+		f_var = 0.0f;
+		//倍精度浮動小数点数型
+		d_var = 0.0d;
+		//文字型
+		c_var = '\u0000';
+		//文字列型
+		S_var = null;
+		//ブーリアン型
+		b_var = false;
+		
+		//バイト型
+		var = 10;
+		//短整数型
+		s_var = 100;
+		//整数型
+		i_var = 1000;
+		//長整数型
+		l_var = 10000l;
+		//単精度浮動小数点数型
+		f_var = 9.5f;
+		//倍精度浮動小数点数型
+		d_var = 10.5d;
+		//文字型
+		c_var = 'a';
+		//文字列型
+		S_var = "ハロー";
+		//ブーリアン型
+		b_var = true;
+		
+		System.out.println(var+s_var+i_var+l_var);
+	}
+
+}
+
